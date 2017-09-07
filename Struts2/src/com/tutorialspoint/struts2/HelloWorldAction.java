1b084686315d32357f970cf530eef98e969cc927
inokou <inokou@192.168.78.100>
2017-09-07 15:20:50 +0900

    ブランチ作成用一時変更

--- Struts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ Struts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -2,33 +2,35 @@ package com.tutorialspoint.struts2;
 
 import org.apache.log4j.Logger;
 
-public class HelloWorldAction {
+import com.tutorialspoint.struts2.dto.NameList;
+
+public class HelloWorldAction extends NameList {
 
 	// ①Loggerを取得
 	private static Logger logger = Logger.getLogger("HelloWorldAction");
 
-		private String name;
-		private String address;
+//		private String name;
+//		private String address;
 
 	   public String execute() throws Exception {
 		   logger.info("テスト開始ログ");
 		   return "success";
 	   }
 
-	   public String getName() {
-		  return name;
-	   }
-
-	   public void setName(String name) {
-		  this.name = name;
-	   }
-
-		public String getAddress() {
-			return address;
-		}
-
-		public void setAddress(String address) {
-			this.address = address;
-		}
+//	   public String getName() {
+//		  return name;
+//	   }
+//
+//	   public void setName(String name) {
+//		  this.name = name;
+//	   }
+//
+//		public String getAddress() {
+//			return address;
+//		}
+//
+//		public void setAddress(String address) {
+//			this.address = address;
+//		}
 
 }
