ce9217c1850d1898a39074840abea325293129fb
inokou <inokou@10.229.72.48>
2017-09-19 18:01:26 +0900

    ログ入れてみたけど…

--- Struts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
+++ Struts2/src/com/tutorialspoint/struts2/HelloWorldAction.java
@@ -1,30 +1,44 @@
 package com.tutorialspoint.struts2;
 
+import org.apache.logging.log4j.LogManager;
+
 import com.tutorialspoint.struts2.chek.UserLogin;
 import com.tutorialspoint.struts2.dto.NameList;
 
-
+/**
+ *
+ * ログ出力テストメイン
+ *
+ * @author inokou
+ *
+ */
 public class HelloWorldAction extends NameList {
 
-	   public String execute() throws Exception {
+	private final org.apache.logging.log4j.Logger logger = LogManager.getLogger(HelloWorldAction.class);
 
-		   UserLogin userLogin = new UserLogin();
+	public String execute() throws Exception {
 
-		   //入力値チェック
-		   boolean adderesRet = userLogin.addressSet(this.getAddress());
-		   boolean nameRet = userLogin.nameSet(this.getName());
 
-		   if(adderesRet == true && nameRet == true){
-			   this.setRetMessage("成功でーーーす");
-			   return "success";
-		   }else{
-			   this.setRetMessage("つまりは失敗ってこと！[住所=" + adderesRet + "][名前=" + nameRet + "]");
-			   return "failure";
-		   }
+		logger.trace("TraceMessage");
+		logger.debug("DebugMessage");
 
 
 
+		UserLogin userLogin = new UserLogin();
 
-	   }
+		// 入力値チェック
+		boolean adderesRet = userLogin.addressSet(this.getAddress());
+		boolean nameRet = userLogin.nameSet(this.getName());
+
+		if (adderesRet == true && nameRet == true) {
+			this.setRetMessage("成功でーーーす");
+			return "success";
+		} else {
+			this.setRetMessage("つまりは失敗ってこと！[住所=" + adderesRet + "][名前="
+					+ nameRet + "]");
+			return "failure";
+		}
+
+	}
 
 }
